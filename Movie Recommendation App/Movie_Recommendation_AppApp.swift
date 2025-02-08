c5abdf9588de9900ac22ede888e4130917905eae
MortezaSaf12 <morsaf731@gmail.com>
2025-02-09 00:39:40 +0100

    Initial Commit

---
+++ Movie Recommendation App/Movie_Recommendation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Recommendation_AppApp.swift
+//  Movie Recommendation App
+//
+//  Created by Morteza Safari on 2025-02-09.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Recommendation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movie Recommendation AppTests/Movie_Recommendation_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Recommendation_AppTests.swift
+//  Movie Recommendation AppTests
+//
+//  Created by Morteza Safari on 2025-02-09.
+//
+
+import Testing
+@testable import Movie_Recommendation_App
+
+struct Movie_Recommendation_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
