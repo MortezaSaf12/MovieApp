7788c2385339dab2a19843b4d2c7d7fa9a7479ba
MortezaSaf12 <morsaf731@gmail.com>
2025-02-17 15:17:35 +0100

    API

--- Movie Recommendation App/Models/Movies.swift
+++ Movie Recommendation App/Models/Movies.swift
@@ -9,7 +9,19 @@ import Foundation
 import SwiftData
 
 
+// Movie Entity
 @Model
 final class Movie {
+    var title: String
+    var genre: String
+    var tmdbID: Int // Unique identifier from OMDP
     
+    // Link to the User who saved this movie
+    var user: User?
+    
+    init(title: String, genre: String, tmdbID: Int) {
+        self.title = title
+        self.genre = genre
+        self.tmdbID = tmdbID
+    }
 }
--- Movie Recommendation App/Models/Users.swift
+++ Movie Recommendation App/Models/Users.swift
@@ -8,7 +8,15 @@
 import Foundation
 import SwiftData
 
+// User Entity
 @Model
 final class User {
-
+    var name: String
+    var preferences: [String]
+    @Relationship(deleteRule: .cascade) var savedMovies: [Movie] = [] // One-to-many relationship, a user can save many Movie objects
+    
+    init(name: String, preferences: [String]) {
+        self.name = name
+        self.preferences = preferences
+    }
 }
